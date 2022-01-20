a9567096be9861b08daacba6c4b6d5a5ed42a56c
Julien Buonarota <j.buohk@gmail.com>
2022-01-20 17:46:37 +0100

    Succesful check of process execution with arbitrary time entry
    Scuccesfully calculate the date of last exec accepted based on time
    difference with current date. Does not return true or false yet or
    append a record of the exec.

--- processLogger.sh
+++ processLogger.sh
@@ -36,7 +36,6 @@ while getopts "hY:m:d:H:M:S:p:" OPTION; do
 	    exit 1;;
     esac
 done
-# TODO decide where (and how) to calculate the most_recent_date_accepted
 
 #default value of 1 day elaspe time
 year=${year:-0}
@@ -46,45 +45,33 @@ hour=${hour:-0}
 minute=${minute:-0}
 second=${second:-0}
 
-# lets separate date and hour
-most_recent_date_accepted=$(($year*10000 + $month*100 + $day*1))
-most_recent_hour_accepted=$(($hour*10000 + $minute*100 + $second*1))
+most_recent_date_accepted_epoch=$(date -d "-$year year -$month month -$day day -$hour hour -$minute minute -$second second" +%s)
 
-# echo $most_recent_date_accepted
-# echo $most_recent_hour_accepted
-# 
-# echo $(expr ${year:-0} + ${carry_year:-0})
-# echo $(expr ${month:-0} + ${carry_month:-0})
-# echo $(expr ${day:-1} + ${carry_day:-0})
-# echo $(expr ${hour:-0} + ${carry_hour:-0})
-# echo $(expr ${minute:-0} + ${carry_minute:-0})
-# echo $(expr ${second:-0} + ${carry_second:-0})
-
-# IDEA use -u option to get time as universal time zone so that the
-# excution of this script does not depend of local environment
-
-current_date=$(date +%Y%m%d)
-current_hour=$(date %H%M%S)
 # parse the last process timelapse condition
 #  by default : 1 day
-# check if the process has been executed
 log_file_name="testProcessLog.log"
 
 # process_name="ananas"
-# most_recent_date_accepted=20200101010101
 latest_process_exec=$(grep $process_name, $log_file_name | \
 			  cut --delimiter="," --field=2 | \
 			  sort --numeric-sort --reverse | \
 			  head --lines=1)
 
 echo "latest process exec : " $latest_process_exec
-echo "most recent data accepted : " $most_recent_date_accepted
+echo "latest process exec in epoch : " $(date -d "$latest_process_exec" +"%s")
+echo "most recent data accepted  : " $(date -d @$most_recent_date_accepted_epoch +"%Y_%m_%d %H:%M:%S")
+echo "most recent data accepted in epoch : " $most_recent_date_accepted_epoch
 
-if $(test $most_recent_date_accepted -gt $latest_process_exec)
+latest_process_exec_epoch=$(date -d "$latest_process_exec" +"%s")
+
+if $(test $most_recent_date_accepted_epoch -gt $latest_process_exec_epoch)
 then echo "process has not been executed yet"
 else echo "process already executed"
 fi
 
+# TODO change above condition to true or false return
+# TODO add record if process has to be executed
+# TODO option to delete the X nb of record, in up or down order
 
 # TODO display if process have ever been recorded in this .log
 # IMPLEMENTATION if $(grep -c process_name a_file) = 0
