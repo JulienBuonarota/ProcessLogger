c8c9b49d52593266faa985ac17648d9fcb6b5f9c
Julien Buonarota <j.buohk@gmail.com>
2022-05-13 22:39:29 +0200

    try to fix the Readme to show up properly on github

--- python_exec.py
+++ python_exec.py
@@ -1,9 +1,34 @@
 import subprocess
+import os
+
+# TODO resolve problem of log file being save outside the exec path
+def ananas_process():
+    print("process to be executed")
 
 ## Verify if the process called ananas has been executed in the last 2 seconds
-output = subprocess.run("./processLogger.sh -p ananas -S 2", shell=True, capture_output=True)
-# cast to bool does not work
-# TODO check how to handle b""
-process_executed = eval(output.stdout.decode().strip())
-print(output)
-print(process_executed)
+log_file_path = os.path.join(os.getcwd(), "example_log.log")
+output = subprocess.run("./processLogger.sh -f {} -p ananas -d 2".format(log_file_path),
+                        shell=True, capture_output=True)
+
+## Parsing of the different possible outputs
+output_string = output.stdout.decode().strip()
+if 'process' in output_string:
+    # example of possible output_string:
+    # No process of name -  ananas  - have been found in log file -  example_log.log  -
+    print(output_string)
+    # can consider that if the process has not been log yet if should be executed
+    output_bool = False
+elif ('True' in output_string) or ('False' in output_string):
+    output_bool = eval(output_string)
+else:
+    print("unknown output")
+
+# exec
+if output_bool is False:
+    # the process has not been executed yet
+    ananas_process()
+    # record the execution of the process
+    output = subprocess.run("./processLogger.sh -l -f example_log.log -p ananas", shell=True, capture_output=True)
+else:
+    print("No execution of process ananas needed")
+
